dacc10cd9ed65c87ef9aefd2ee70433383e05acd
Matthew Terwilliger <matt@terwilligers.com>
2019-01-23 20:31:18 -0500

    Add a few homepage photos to the rotation

--- assets/js/random_image.js
+++ assets/js/random_image.js
@@ -1,4 +1,15 @@
 $(function() {
-	var images = ['temp-truck.jpg', 'stations-01.jpg', 'stations-02.jpg', 'eta-497.jpg', 'er-493.jpg', 'two-trucks.jpg'];
+  var images = [
+    'temp-truck.jpg',
+    'stations-01.jpg',
+    'stations-02.jpg',
+    'eta-497.jpg',
+    'er-493.jpg',
+    'two-trucks.jpg',
+    'IMG_0147.JPG',
+    'IMG_0191.JPG',
+    'IMG_8583.JPG',
+  ];
+
 	$('<img src="assets/img/home-rotate/' + images[Math.floor(Math.random() * images.length)] + '" width=100% >').appendTo('#banner');
 });
